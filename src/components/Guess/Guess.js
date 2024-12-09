176d2eff9781391813ab92e6f3939dccf7507a53
Max I <clickkava@gmail.com>
2024-12-09 14:43:36 +0200

    Game logic

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import { range } from '../../utils'
+import { checkGuess } from '../../game-helpers'
 
-const Guess = ({ value }) => {
+const Guess = ({ value, answer }) => {
+  let checkedGuess;
+  if (value) {
+    checkedGuess = checkGuess(value, answer);
+  }
+  
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
+        <span key={num} className={`cell ${value && checkedGuess[num].status}`}>
           {value ? value[num] : undefined}
         </span>
       ))}
--- src/components/GuessResults/GuessResults.js
+++ src/components/GuessResults/GuessResults.js
@@ -3,11 +3,11 @@ import Guess from '../Guess'
 import { range } from '../../utils'
 import { NUM_OF_GUESSES_ALLOWED } from '../../constants'
 
-const GuessResults = ({ guesses }) => {
+const GuessResults = ({ guesses, answer }) => {
   return (
     <div className="guess-results">
       {range(NUM_OF_GUESSES_ALLOWED).map((num) => (
-        <Guess key={num} value={guesses[num]} />
+        <Guess key={num} value={guesses[num]} answer={answer} />
       ))}
     </div>
   )
